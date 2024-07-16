c490c1cd29f83271f9f89a313dd7d8b53a814e77
Kasumi Akiyama★ <shellyakiyama@gmail.com>
2024-07-17 05:39:05 +0900

    fixed errors

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -7,7 +7,7 @@ const initdb = async () =>
         console.log('editor database already exists');
         return;
       }
-      db.createObjectStore('edtor', { keyPath: 'id', autoIncrement: true });
+      db.createObjectStore('editor', { keyPath: 'id', autoIncrement: true });
       console.log('editor database created');
     },
   });
@@ -25,15 +25,16 @@ export const putDb = async (content) =>
     const store = tx.objectStore('editor');
 
       // Use the .add() method on the store and pass in the content.
-    const request = store.add({content: content});
+    const request = store.put({content: content, id:1});
     const result = await request;
     console.log('Data saved to the database', result);
-  }
+  };
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
+  try{
     // Create a connection to the database database and version we want to use.
-    const editorDb = await openDB('editor', 1);
+     const editorDb = await openDB('editor', 1);
 
     // Create a new transaction and specify the database and data privileges.
     const tx = editorDb.transaction('editor', 'readonly');
@@ -42,11 +43,14 @@ export const getDb = async () => {
     const store = tx.objectStore('editor');
   
     // Use the .getAll() method to get all data in the database.
-    const request = store.getAll();
+    const request = store.get(1);
   
     // Get confirmation of the request.
     const result = await request;
     console.log('result.value', result);
-    return result;
+    return result?.content;
+  }catch(err){
+    console.error('getDb error: ' + err);
+  }
 }
 initdb();
